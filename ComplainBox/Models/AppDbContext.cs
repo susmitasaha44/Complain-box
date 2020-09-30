a88d76b7ce36e19bdfbe62df7ca73c610e8fc0b5
Susmita Saha <susmita35-1544@diu.edu.bd>
2020-10-01 00:23:30 +0600

    v-1

---
+++ ComplainBox/Models/AppDbContext.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ComplainBox.Models
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+        {
+
+        }
+
+        public DbSet<UserAccount> userAccounts { get; set; }
+
+        public DbSet<Complain> complains { get; set; }
+
+        public DbSet<Note> notes { get; set; }
+
+        public DbSet<Appointment> appointments { get; set; }
+    }
+}
---
+++ ComplainBox/Models/Complain.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ComplainBox.Models
+{
+    public class Complain
+    {
+        [Key]
+        public int ComplainId { get; set; }
+
+        [Display(Name = "Name")]
+        public string Name { get; set; }
+
+        [Display(Name = "Complain date")]
+        [DataType(DataType.Date)]
+        //[Column(TypeName = "date")]
+        public DateTime? ComplainDate { get; set; }
+
+        [Display(Name = "Complain Title")]
+        public string ComplainTitle { get; set; }
+
+        [Display(Name = "Description")]
+        public string ComplainDescription { get; set; }
+
+        [Display(Name = "Deadline")]
+        [DataType(DataType.Date)]
+        [Column(TypeName = "date")]
+        public DateTime? Deadline { get; set; }
+
+        public string Status { get; set; }
+
+        [Display(Name = "Assigner Name")]
+        public int UserId { get; set; }
+
+        public virtual UserAccount UserAccount { get; set; }
+    }
+}
